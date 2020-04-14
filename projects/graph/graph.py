239fb0580c85cad1e4d90625d21769c21aedac35
Jengopockets <jengopockets@gmail.com>
2020-04-14 14:03:17 -0400

    add vertex and edge working

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -7,25 +7,34 @@ class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        self.vertices = {}
+        self.vertices = {} #Ajacency List
 
     def add_vertex(self, vertex_id):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
+
 
     def add_edge(self, v1, v2):
         """
-        Add a directed edge to the graph.
+        Add a directed edge to the graph from v1 to v2
         """
-        pass  # TODO
+        # check if they exist
+        if v1 in self.vertices and v2 in self.vertices:
+            #add the edge
+            self.vertices[v1].add(v2)
+        else:
+            print("Vertex not found")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        if vertex_id in self.vertices:
+            return self.vertices[vertex_id]
+        else:
+            return None
 
     def bft(self, starting_vertex):
         """
